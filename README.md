3266ec926063564a0580ef09dd0b1e3821956678
dimitris orfanoudakis <dimitrisorfanuakis@gmail.com>
2024-09-20 21:11:43 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# delty-grpc
---
+++ delty_grpc/__init__.py
@@ -0,0 +1,4 @@
+from .track_content_difference_pb2 import TriggerRequest, TriggerResponse
+from .track_content_difference_pb2_grpc import TrackContentDifferenceServiceStub
+
+__all__ = ['TriggerRequest', 'TriggerResponse', 'TrackContentDifferenceServiceStub']
